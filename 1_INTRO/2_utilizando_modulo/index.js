088bc15f3b10158a451b4603f2760aa566530f8b
Felipe Nascimento <felipeplnasc92@gmail.com>
2026-01-21 10:40:29 -0300

    felipe test changes

--- 1_INTRO/2_utilizando_modulo/index.js
+++ 1_INTRO/2_utilizando_modulo/index.js
@@ -2,7 +2,7 @@ const fs = require('fs') //file system
 
 fs.readFile('arquivo1.txt', 'utf8', (err, data) => {
     if (err) {
-        console.log(err)
+        console.log(err + " - Arquivo não encontrado")
         return
     }
     console.log(data)
